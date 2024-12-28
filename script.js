7fa82b5fe01e36ee0210b7cedc5f3b4c6c6bbe36
sabal0321 <sabalthapa0321@gmail.com>
2024-12-28 19:51:05 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+fullName="Sabal Thapa";
+console.log(fullName);
+var countryName="Nepal";
+console.log(countryName);
+let cityName="kathmandu";
+console.log(cityName);
+const appName="My project";
+console.log(appName);
