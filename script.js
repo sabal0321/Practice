1d8bcb51888bd6ae048b80f24faf63595dc99d01
sabal0321 <sabalthapa0321@gmail.com>
2024-12-28 20:06:48 +0545

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-fullName="Sabal Thapa";
-console.log(fullName);
-var countryName="Nepal";
-console.log(countryName);
-let cityName="kathmandu";
-console.log(cityName);
-const appName="My project";
-console.log(appName);
